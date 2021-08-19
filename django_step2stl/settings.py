4c1636e5c385f8ce39d70154a6ef6a5fe4db75a8
Vital Hegner <vhegner@dotworks.ch>
2021-08-19 09:09:09 +0200

    changes for deployment

--- django_step2stl/settings.py
+++ django_step2stl/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eu_2p8=2ytt(rvv@lu!p059yi@w(bi=*rxs5ik_sj0z0+gz%ah'
+if os.environ.get('DJANGO_DEBUG', '') != 'True':
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+else:
+    SECRET_KEY = 'django-insecure-eu_2p8=2ytt(rvv@lu!p059yi@w(bi=*rxs5ik_sj0z0+gz%ah'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
